05b3b737c29921f516070e1112c7eaaf36eddcda
Bianca Gonçalves <bianca.carvalho@picpay.com>
2020-03-25 01:08:46 -0300

    feat: pagination on profile page

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -4,10 +4,18 @@ module.exports = {
   async index(req, res) {
     const ong_id = req.headers.authorization;
 
+    const { page = 1 } = req.query;
+    // gett total of incidents
+    const [count] = await connection('incidents').count();
+
     const incidents = await connection('incidents')
       .where('ong_id', ong_id)
+      .limit(5)
+      .offset((page - 1) * 5)
       .select('*');
 
+    // send total on header
+    res.header('X-Total-Count', count['count(*)']);
     return res.json(incidents);
   },
 };

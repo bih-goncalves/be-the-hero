b27bdcb6dd87e284dc5cf8146665ca08794d3605
Bianca Gonçalves <bianca.carvalho@picpay.com>
2020-03-25 00:33:57 -0300

    feat: delete incidents route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,17 @@
 const express = require('express');
 
 const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
 
 const routes = express.Router();
 
+// for ongs
 routes.get('/ongs', OngController.index);
-
 routes.post('/ongs', OngController.create);
+
+// for incidents
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;

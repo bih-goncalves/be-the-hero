9f349a25f01fe120c225c9578fc964e59b6847ea
Bianca Gonçalves <bianca.carvalho@picpay.com>
2020-03-25 01:06:39 -0300

    feat: pagination on incidents

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -2,8 +2,19 @@ const connection = require('../database/connection');
 
 module.exports = {
   async index(req, res) {
-    const incidents = await connection('incidents').select('*');
+    const { page = 1 } = req.query;
 
+    // gett total of incidents
+    const [count] = await connection('incidents').count();
+
+    // get a page of incidents
+    const incidents = await connection('incidents')
+      .limit(5)
+      .offset((page - 1) * 5)
+      .select('*');
+
+    // send total on header
+    res.header('X-Total-Count', count['count(*)']);
     return res.json(incidents);
   },
   async create(req, res) {

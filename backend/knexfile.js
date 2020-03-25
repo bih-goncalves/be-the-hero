b7b667cda2ec11cf49030fed3339ec8e33fda524
Bianca Gonçalves <bianca.carvalho@picpay.com>
2020-03-24 22:34:46 -0300

    feat: adding database conector config

---
+++ backend/knexfile.js
@@ -0,0 +1,54 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'mysql',
+    version: '5.7',
+    connection: {
+      host: 'db.backend.dev:3306',
+      database: 'backend',
+      user: 'root',
+      password: 'root',
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './src/database/migrations',
+    },
+  },
+
+  staging: {
+    client: 'mysql',
+    version: '5.7',
+    connection: {
+      host: 'db.backend.dev:3306',
+      database: 'backend',
+      user: 'root',
+      password: 'root',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+
+  production: {
+    client: 'mysql',
+    version: '5.7',
+    connection: {
+      host: 'db.backend.dev:3306',
+      database: 'backend',
+      user: 'root',
+      password: 'root',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+};

49a018145ca176e2a3335a0fc0f940464edb1f36
Bianca Gonçalves <bianca.carvalho@picpay.com>
2020-03-28 19:33:45 -0300

    feat: incidents page

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,9 +1,63 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation, NavigationContainer } from '@react-navigation/native';
+import { View, FlatList, Image, Text, TouchableOpacity } from 'react-native';
 
-import { View } from 'react-native';
-
-// import { Container } from './styles';
+import styles from './styles';
+import logoImg from '../../assets/logo.png';
 
 export default function Incidents() {
-  return <View />;
+  const navigation = useNavigation();
+
+  function navigateToDetail() {
+    navigation.navigate(`Detail`);
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logoImg} />
+        <Text style={styles.headerText}>
+          Toral de <Text style={styles.headerTextBold}>0 casos.</Text>
+        </Text>
+      </View>
+      {/* título topo */}
+      <Text style={styles.title}>Bem-vindo!</Text>
+      <Text style={styles.description}>
+        Escolha um dos casos abaixo e salve o dia.
+      </Text>
+
+      <FlatList
+        data={[1, 2, 3, 4, 5]}
+        style={styles.incidentList}
+        keyExtractor={incident => String(incident)}
+        showsVerticalScrollIndicator={false}
+        renderItem={() => (
+          <View style={styles.incident}>
+            <View style={styles.caseTop}>
+              <View>
+                <Text style={styles.incidentProperty}>CASO:</Text>
+                <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+              </View>
+
+              <View>
+                <Text style={styles.incidentProperty}>ONG:</Text>
+                <Text style={styles.incidentValue}>APAD</Text>
+              </View>
+            </View>
+            <Text style={styles.incidentProperty}>VALOR:</Text>
+            <Text style={styles.incidentValue}>R$ 120,00</Text>
+
+            <TouchableOpacity
+              style={styles.detailsButton}
+              onPress={navigateToDetail}
+            >
+              <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+              <Feather name="arrow-right" size={16} color="#E02041" />
+            </TouchableOpacity>
+          </View>
+        )}
+      />
+    </View>
+  );
 }

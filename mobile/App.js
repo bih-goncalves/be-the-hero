c61cafaae7fb41874f6a7ba67d73e9dc42cd55f1
Bianca Gonçalves <bianca.carvalho@picpay.com>
2020-03-28 19:33:22 -0300

    chore: routes and internal images for app

--- mobile/App.js
+++ mobile/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './src/routes';
 
 export default function App() {
-  return (
-    <View>
-      <Text>Bora codar!</Text>
-    </View>
-  );
+  return <Routes />;
 }

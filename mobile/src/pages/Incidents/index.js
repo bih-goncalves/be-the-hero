6dda31cfcfe1fe524d7e9eb42c3697baf550e3af
Bianca Gonçalves <bianca.carvalho@picpay.com>
2020-03-28 08:14:22 -0300

    feat: Incident pages

---
+++ mobile/src/pages/Incidents/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function Incidents() {
+  return <View />;
+}
